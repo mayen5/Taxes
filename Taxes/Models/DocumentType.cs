fed749dd0af62c12f73681d2ec2987a02e8238e5
Carmelo Mayen <carmelo5mayen@gmail.com>
2019-06-02 18:56:06 -0600

    Document Types

---
+++ Taxes/Models/DocumentType.cs
@@ -0,0 +1,17 @@
+﻿namespace Taxes.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    public class DocumentType
+    {
+        [Key]
+        public int DocumentTypeId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Index("DocumentType_Description_Index", IsUnique = true)]
+        [StringLength(30, ErrorMessage = "The field {0} can contain maximun {1} and minimum {2} characters",
+            MinimumLength = 1)]
+        public String Description { get; set; }
+    }
+}
--- Taxes/Models/TaxesContext.cs
+++ Taxes/Models/TaxesContext.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
-
-namespace Taxes.Models
+﻿namespace Taxes.Models
 {
+    using System.Data.Entity;
     public class TaxesContext : DbContext
     {
         public TaxesContext() : base("DefaultConnection")
@@ -20,8 +15,10 @@ namespace Taxes.Models
 
         public DbSet<PropertyType> PropertyTypes { get; set; }
 
-        public System.Data.Entity.DbSet<Taxes.Models.Department> Departments { get; set; }
+        public DbSet<Department> Departments { get; set; }
 
-        public System.Data.Entity.DbSet<Taxes.Models.Municipality> Municipalities { get; set; }
+        public DbSet<Municipality> Municipalities { get; set; }
+
+        public DbSet<DocumentType> DocumentTypes { get; set; }
     }
 }
